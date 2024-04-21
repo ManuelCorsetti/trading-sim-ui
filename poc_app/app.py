73f514dcada979c3b27514ac30adbb25a76aca7d
ManuelCorsetti <42147538+ManuelCorsetti@users.noreply.github.com>
2024-04-21 12:03:23 +0100

    Added POC docker and poetry

--- poc_app/app.py
+++ poc_app/app.py
@@ -2,14 +2,17 @@
 import streamlit as st
 from pages import landing, result
 
+st.set_page_config(page_title="POC: Visualize Strategy Performance", page_icon="📈")
+
+
 PAGES = {
     "Landing": landing,
     "Results": result,
 }
 
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+# st.sidebar.title('Navigation')
+# selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
-page = PAGES[selection]
-page.app()
+# page = PAGES[selection]
+# page.app()
 
