fca44bcf0aa35ac1af3143299d1db22cf4b774ee
ManuelCorsetti <42147538+ManuelCorsetti@users.noreply.github.com>
2024-04-21 20:12:32 +0100

    test

--- poc_app/app.py
+++ poc_app/app.py
@@ -10,7 +10,7 @@ PAGES = {
     "Results": result,
 }
 
-# st.sidebar.title('Navigation')
+st.sidebar.title('Navigation')
 # selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
 # page = PAGES[selection]
